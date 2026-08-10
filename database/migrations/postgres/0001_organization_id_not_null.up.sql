7ef30881bca5206a3c8f647db147f51159b85b00
zowork <sdkwork@zowalk.com>
2026-08-10 19:00:15 +0800

    chore: sync local changes

--- database/migrations/postgres/0001_organization_id_not_null.up.sql
+++ database/migrations/postgres/0001_organization_id_not_null.up.sql
@@ -18,22 +18,27 @@
 
 BEGIN;
 
+ALTER TABLE browser_session ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE browser_session SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE browser_session ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE browser_session ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE browser_bookmark ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE browser_bookmark SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE browser_bookmark ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE browser_bookmark ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE browser_history ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE browser_history SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE browser_history ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE browser_history ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE browser_tab ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE browser_tab SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE browser_tab ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE browser_tab ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE browser_download ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE browser_download SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE browser_download ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE browser_download ALTER COLUMN organization_id SET NOT NULL;
